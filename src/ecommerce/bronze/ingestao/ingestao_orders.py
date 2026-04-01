79b88cc41ded55b93af194775e629ae2c81fd75d
ademirmendonca <ademir_mendonca@hotmail.com>
2026-04-01 02:57:20 +0000

    chore: primeiro commit

---
+++ src/ecommerce/bronze/ingestao/ingestao_orders.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (StructType, StructField, StringType, TimestampType)
+from pyspark.sql import functions as F
+
+
+class BronzeIngestion:
+    def __init__(self, app_name="list_orders"):
+        self.spark = SparkSession.builder.appName(app_name).getOrCreate()
+        print("Iniciando processamento..")
+
+    def ingestao_bronze(self, file_path, catalogo_path, tipo_carga):
+        print(f"Processando arquivo: {file_path}, carga: {tipo_carga}")
+
+        schema = StructType([
+            StructField("order_id", StringType(), True),
+            StructField("customer_id", StringType(), True),
+            StructField("order_status", StringType(), True),
+            StructField("order_purchase_timestamp", TimestampType(), True),
+            StructField("order_approved_at", TimestampType(), True),
+            StructField("order_delivered_carrier_date", TimestampType(), True),
+            StructField("order_delivered_customer_date", TimestampType(), True),
+            StructField("order_estimated_delivery_date", TimestampType(), True),
+            StructField("ingestion_timestamp", TimestampType(), True)
+        ])
+
+        df = self.spark.read.csv(file_path, schema=schema, header=True, sep=",")
+
+        df = (
+            df.withColumn("ingestion_timestamp", F.current_timestamp())
+        )
+
+        df.limit(5).display()
+
+        #df.write.mode("append").saveAsTable(catalogo_path)
+
+
