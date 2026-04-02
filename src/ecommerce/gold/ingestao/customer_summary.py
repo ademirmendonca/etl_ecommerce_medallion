3179df34482632eac6a62d8829e7c81ebffcab09
Ademir Mendonça <ademir_mendonca@hotmail.com>
2026-04-02 10:48:06 -0300

    feat: Agregacoes camada Gold (#5)

---
+++ src/ecommerce/gold/ingestao/customer_summary.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+class ETLGold:
+    def __init__(self, app_name="etl-gold-customer-summary"):
+        self.spark = SparkSession.builder.appName(app_name).getOrCreate()
+        print("Iniciando processamento, customer-summary..")
+
+    def ingestao_gold(self, df_consolidate, df_payments, catalogo_destino, tipo_carga):
+        print(f"Processando novos dados, carga: {tipo_carga}")
+
+        try:
+
+            df_final = (
+                df_consolidate.join(df_payments, df_consolidate.order_id == df_payments.order_id)
+                .groupBy(
+                    df_consolidate.customer_id.alias("customer_id"),
+                    df_consolidate.customer_city.alias("customer_city"),
+                    df_consolidate.customer_state.alias("customer_state")
+                )
+                .agg(
+                    F.count(df_consolidate.order_id).alias("total_orders"),
+                    F.sum(df_consolidate.price + df_consolidate.freight_value).alias("total_revenue"),
+                    F.round(F.avg(df_consolidate.price + df_consolidate.freight_value), 2).alias("avg_order_value")
+                )
+            )
+
+            (
+                df_final
+                .write
+                .format("delta")
+                .mode("append")
+                .saveAsTable(catalogo_destino)
+            )
+            print(f"Processamento finalizado com sucesso em: {catalogo_destino}")
+        except Exception as e:
+            print(f"Erro ao processar novos dados: {e}")
+
+
---
+++ src/ecommerce/gold/ingestao_executor/gold_product_summary_executor.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from src.ecommerce.gold.ingestao.product_summary import ETLGold
+
+spark = SparkSession.getActiveSession()
+
+# Ambiente
+ambiente = "prd"  # prd / dev
+
+# Fonte
+df_consolidate = spark.read.table(f"workspace.silver_{ambiente}.orders_consolidated")
+df_payments = spark.read.table(f"workspace.silver_{ambiente}.payments_summary")
+
+
+# Destino
+workspace = "workspace"
+schema = f"gold_{ambiente}"
+tabela = "product_summary"
+catalogo_destino = f"{workspace}.{schema}.{tabela}"
+
+# Tipo de carga
+if spark.catalog.tableExists(catalogo_destino):
+    tipo_carga = "incremental"
+else:
+    print(f"Tabela não existe, criando.. {catalogo_destino}")
+    tipo_carga = "full"
+
+
+def main():
+    pipe = ETLGold()
+    pipe.ingestao_gold(
+        df_consolidate=df_consolidate,
+        df_payments=df_payments,
+        catalogo_destino=catalogo_destino,
+        tipo_carga=tipo_carga
+    )
+
+if __name__ == "__main__":
+    main()
+
